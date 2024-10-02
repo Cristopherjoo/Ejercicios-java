ebf18e9fde56f122955635f7d3057ff8934f91c3
Cristopher <cristopherjoo@gmail.com>
2024-10-02 03:46:04 +0000

    Añadir App.java

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Mis primeros pasos en JAVA.");
+        System.out.println("Le gustaria pasar a tomar una tacita de cafe.");
+    }
+}
